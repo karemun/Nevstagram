121f580d4a2c2d88e3c41f2d9cb2e7002b7b91e7
Karen Flores <karemu.8910@gmail.com>
2023-03-13 01:58:32 -0600

    Mostrar Posts individuales

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -64,4 +64,11 @@ class PostController extends Controller
 
         return redirect()->route('posts.index', auth()->user()->username);
     }
+
+    public function show(User $user, Post $post)
+    {
+        return view('posts.show', [
+            'post' => $post
+        ]);
+    }
 }
